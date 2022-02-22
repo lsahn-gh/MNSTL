11202d47adff08e0f5aaf79c8e4c7049f77bb47e
Leesoo Ahn <lsahn@ooseel.net>
2022-02-22 21:53:21 +0900

    mnstl/fixed-vector.h: implement default FixedVector methods
    Signed-off-by: Leesoo Ahn <lsahn@ooseel.net>

---
+++ include/mnstl/fixed-vector.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <iostream>
+#include <cassert>
+
+namespace mnstl
+{
+
+template <typename T, size_t N>
+class FixedVector
+{
+public:
+    FixedVector();
+
+    bool Add(const T& value);
+    void Clear();
+    size_t GetSize() const;
+    size_t GetCapacity() const;
+
+    T& operator[](const int index);
+
+private:
+    size_t mSize;
+    T mArray[N];
+};
+
+// Implementations
+template <typename T, size_t N>
+FixedVector<T, N>::FixedVector()
+    : mSize(0)
+{
+}
+
+template <typename T, size_t N>
+bool FixedVector<T, N>::Add(const T& value)
+{
+    if (mSize >= N)
+    {
+        return false;
+    }
+
+    mArray[mSize++] = value;
+    return true;
+}
+
+template <typename T, size_t N>
+void FixedVector<T, N>::Clear()
+{
+    mSize = 0;
+}
+
+template <typename T, size_t N>
+size_t FixedVector<T, N>::GetSize() const
+{
+    return mSize;
+}
+
+template <typename T, size_t N>
+size_t FixedVector<T, N>::GetCapacity() const
+{
+    return N;
+}
+
+template <typename T, size_t N>
+T& FixedVector<T, N>::operator[](const int index)
+{
+    assert((0 <= index) && (index < N));
+    /* should we deal with 'mSize == 0' case? */
+
+    return mArray[index];
+}
+
+} /* mnstl namespace */
